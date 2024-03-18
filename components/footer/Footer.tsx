ecc4bf97469583261c4cb5a51b358061960616fa
JEaLiFe Jeaguy <jeanguylanememiaghe@gmail.com>
2024-03-18 03:13:51 +0000

    Changes to be committed: 	new file:   app/dashboard.png 	modified:   app/globals.css 	modified:   app/layout.tsx 	modified:   app/page.tsx 	new file:   components/footer/Footer.tsx 	new file:   components/navbar/NavBar.tsx 	new file:   components/sidebar/SideBar.tsx 	modified:   package-lock.json 	modified:   package.json 	new file:   tailwind.config.js 	modified:   tailwind.config.ts

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="rounded-md border-t-[0.2px] border-slate-300 text-slate-100 flex items-center justify-center py-14">Footer</footer>
+  )
+}
---
+++ components/navbar/NavBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav className="h-[calc(100vh-3rem)] w-64 p-1 rounded-md sticky top-6 z-50 border-[0.2px] border-slate-300 text-slate-100 flex flex-col items-center justify-center">
+      NavBar
+    </nav>
+  );
+}
