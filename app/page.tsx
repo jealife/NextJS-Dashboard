1bdfccaffb5340bf63a742d1119ecd567accd320
JEaLiFe Jeaguy <jeanguylanememiaghe@gmail.com>
2024-03-18 03:26:31 +0000

    Changes to be committed: 	modified:   app/layout.tsx 	modified:   app/page.tsx 	modified:   components/footer/Footer.tsx 	modified:   components/navbar/NavBar.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center ">
-      <h1 className="font-bold text-[calc(5.5rem*1)] text-slate-100 text-center">Dashboard</h1>
+      <h1 className="duration-200 font-bold text-[calc(5.5rem*1)] md:text-[calc(5.5rem*0.75)] sm:text-[calc(5.5rem*0.62)] text-slate-100 text-center">Dashboard</h1>
     </div>
   );
 }
--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="rounded-md border-t-[0.2px] border-slate-300 text-slate-100 flex items-center justify-center py-14">Footer</footer>
+    <footer className="rounded-md border-t-[0.2px] border-slate-300 text-slate-100 flex items-center justify-center py-40">Footer</footer>
   )
 }
