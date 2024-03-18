e9d67f4a1ca38697dd6eb52c0a3139c48891f9f2
JEaLiFe Jeaguy <jeanguylanememiaghe@gmail.com>
2024-03-18 03:32:28 +0000

    Changes to be committed: 	modified:   app/layout.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/navbar/NavBar";
 import Footer from "@/components/footer/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
 
 const inter = Inter({ subsets: ["latin"] });
 
