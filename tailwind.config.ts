9a44424088283d8347ae6fa0182e578e35ad43cf
jealife <jeanguylanememiaghe@gmail.com>
2024-03-20 13:33:26 +0100

    Changes to be committed: 	modified:   tailwind.config.js 	modified:   tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,7 @@ module.exports = {
       lg: { max: "«1023px»" },
       // => @media (largeur max: 1023px) { ... }
 
-      md: { max: "767px" },
+      md: { max: "991px" },
       // => @media (largeur max: 767px) { ... }
 
       sm: { max: "639px" },
