606108f6a75c560aef80b6627f6d98e681fa4341
JEaLiFe <83034287+jealife@users.noreply.github.com>
2024-03-20 10:00:41 +0100

    text changer

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="min-h-screen h-[100dvh] flex items-center justify-center ">
       <h1 className=" duration-200 font-bold text-[calc(5.5rem*1)] md:text-[calc(5.5rem*0.75)] sm:text-[calc(5.5rem*0.62)] text-slate-100 text-center">
-        Dashboard
+        Home
       </h1>
     </div>
   );
